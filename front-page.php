57fac245be5a622d6f8dfbe7bec1631ff1880796
Grégoire Noyelle <hello@gregoirenoyelle.com>
2014-10-20 10:23:23 +0200

    Home, temporarily set up to show up the sidebar

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 //* Enqueue scripts
 add_action( 'wp_enqueue_scripts', 'minimum_front_page_enqueue_scripts' );
 function minimum_front_page_enqueue_scripts() {
-	
+
 	//* Load scripts only if custom background is being used
 	if ( ! get_background_image() )
 		return;
@@ -13,7 +13,7 @@ function minimum_front_page_enqueue_scripts() {
 	wp_enqueue_script( 'minimum-backstretch-set', get_bloginfo('stylesheet_directory').'/js/backstretch-set.js' , array( 'jquery', 'minimum-backstretch' ), '1.0.0' );
 
 	wp_localize_script( 'minimum-backstretch-set', 'BackStretchImg', array( 'src' => str_replace( 'http:', '', get_background_image() ) ) );
-	
+
 	//* Add custom body class
 	add_filter( 'body_class', 'minimum_add_body_class' );
 
@@ -32,18 +32,18 @@ function minimum_front_page_genesis_meta() {
 	if ( is_home() ) {
 
 		//* Remove entry meta in entry footer and Genesis loop
-		remove_action( 'genesis_loop', 'genesis_do_loop' );
-		
+		// remove_action( 'genesis_loop', 'genesis_do_loop' );
+
 		//* Add Genesis grid loop
 		add_action( 'genesis_loop', 'minimum_grid_loop_helper' );
-		
+
 		//* Remove entry footer functions
 		remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
 		remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 		remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
-		
+
 		//* Force full width content layout
-		add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+		// add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
 	}
 
