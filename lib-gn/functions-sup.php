180798378edc43f74f72834badb352fb52b95881
Grégoire Noyelle <hello@gregoirenoyelle.com>
2014-10-21 20:59:05 +0200

    Footer loginout correct bug on redirect link

--- lib-gn/functions-sup.php
+++ lib-gn/functions-sup.php
@@ -22,8 +22,8 @@ add_action( 'wp_enqueue_scripts', 'gn_enqueue_js' );
 // perso: footer changer Copyright test
 add_filter('genesis_footer_creds_text', 'perso_texte_copyright');
 function perso_texte_copyright($creds) {
-$creds = 'Copyright '.'[footer_copyright]' . ' :: Création: <a href="http://www.gregoirenoyelle.com/" target="_blank">Grégoire Noyelle</a> :: Base: <a href="http://www.wordpress.org" target="_blank">WordPress</a> , <a href="http://www.studiopress.com/features" target="_blank">Genesis Framework</a>
- :: '. '[footer_loginout redirect="http://www.gregoirenoyelle.com/process/"]';
+$creds = 'Copyright '.'[footer_copyright]' . ' Base: <a href="http://www.wordpress.org" target="_blank">WordPress</a> , <a href="http://www.studiopress.com/features" target="_blank">Genesis Framework</a>
+ :: '. '[footer_loginout]';
 return $creds;
 }
 
