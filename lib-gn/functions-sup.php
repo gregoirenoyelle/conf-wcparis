81707c04dac8611d3903389239f3b247b86b3881
Grégoire Noyelle <hello@gregoirenoyelle.com>
2014-10-22 16:03:28 +0200

    Footer, coopyright g & i

--- lib-gn/functions-sup.php
+++ lib-gn/functions-sup.php
@@ -22,7 +22,7 @@ add_action( 'wp_enqueue_scripts', 'gn_enqueue_js' );
 // perso: footer changer Copyright test
 add_filter('genesis_footer_creds_text', 'perso_texte_copyright');
 function perso_texte_copyright($creds) {
-$creds = 'Copyright '.'[footer_copyright]' . ' Base: <a href="http://www.wordpress.org" target="_blank">WordPress</a> , <a href="http://www.studiopress.com/features" target="_blank">Genesis Framework</a>
+$creds = 'Copyright '.'[footer_copyright]' . ' :: Création <a href="http://www.gregoirenoyelle.com/" target="_blank">Grégoire Noyelle</a>, <a href="http://imathi.eu/" target="_blank">imath</a> :: Base: <a href="http://www.wordpress.org" target="_blank">WordPress</a> , <a href="http://www.studiopress.com/features" target="_blank">Genesis Framework</a>
  :: '. '[footer_loginout]';
 return $creds;
 }
